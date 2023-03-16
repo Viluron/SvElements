dc7d2dfe498393f73bd9845188941edcf33e9bb7
Viluron <markusfiederling@gmail.com>
2023-03-16 20:38:41 +0100

    Added Icon & import Material Icons

---
+++ Icon.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let icon: string;
+	export let color = 'currentColor';
+	export let size = 'unset';
+</script>
+
+<span class="material-symbols-outlined" style={`color: ${color}; font-size: ${size}`}>{icon}</span>
+
+<style scoped>
+	span {
+		color: currentColor;
+	}
+</style>
