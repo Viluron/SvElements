12bb9aefdd696d6b2b078178ba160caf675493f3
Viluron <markusfiederling@gmail.com>
2023-03-16 20:11:03 +0100

    Fancy animations

--- Input.svelte
+++ Input.svelte
@@ -8,25 +8,37 @@
 	export let type: 'text' | 'password' = 'text';
 	export let placeholder: string = undefined;
 	export let autocomplete: 'off' | 'on' = 'off';
+	export let movePlaceholder = true;
+	export let value = '';
 
 	const dispatch = createEventDispatcher();
 
 	let input: HTMLInputElement;
+	let _value: String = '';
 
 	function emit(event: string) {
 		return () => dispatch(event, input.value);
+	}
+
+	function onChange() {
+		_value = input.value;
+		value = input.value;
+		emit('change');
 	}
 </script>
 
 <div class={`custom-input ${className ?? ''}`}>
 	<div class="input-wrapper">
+		{#if movePlaceholder}
+			<span class:up={_value}>{placeholder}</span>
+		{/if}
 		<input
 			bind:this={input}
 			{name}
 			{type}
-			{placeholder}
 			{autocomplete}
-			on:change={emit('change')}
+			placeholder={!movePlaceholder && placeholder ? placeholder : undefined}
+			on:change={onChange}
 			on:input={emit('input')}
 		/>
 		<div class="border" />
@@ -41,6 +53,32 @@
 		justify-content: center;
 		align-items: center;
 		position: relative;
+		font-family: inherit;
+	}
+
+	span {
+		position: absolute;
+		top: 0;
+		left: 0;
+		color: var(--faint-color);
+		transition: all 0.2s ease-out;
+		font-size: inherit;
+		pointer-events: none;
+	}
+
+	span.up {
+		transform: translateY(-100%);
+		font-size: 0.5em;
+	}
+
+	span:has(input:focus) {
+		color: red;
+	}
+
+	span:has(+ input:focus) {
+		color: var(--hover-color);
+		transform: translateY(-100%);
+		font-size: 0.5em;
 	}
 
 	input {
@@ -57,6 +95,7 @@
 
 	input::placeholder {
 		color: var(--faint-color);
+		font-family: inherit;
 	}
 
 	.input-wrapper {
